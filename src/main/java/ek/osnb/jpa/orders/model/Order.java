f9abc1a3b0fda48ee4528b8e43e17009ca331e24
Your Name <you@example.com>
2025-08-29 11:29:39 +0200

    Completed exercise 00 & 01

--- src/main/java/ek/osnb/jpa/orders/model/Order.java
+++ src/main/java/ek/osnb/jpa/orders/model/Order.java
@@ -1,9 +1,15 @@
 package ek.osnb.jpa.orders.model;
 
+import ek.osnb.jpa.common.model.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.time.LocalDate;
 
-public class Order {
-
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity
+{
     private LocalDate orderDate;
 
     private OrderStatus status;
--- src/main/java/ek/osnb/jpa/orders/model/OrderStatus.java
+++ src/main/java/ek/osnb/jpa/orders/model/OrderStatus.java
@@ -1,5 +1,21 @@
 package ek.osnb.jpa.orders.model;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
 public enum OrderStatus {
-    PENDING, SHIPPED, DELIVERED
+    PENDING, SHIPPED, DELIVERED;
+
+    public OrderStatus getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status)
+    {
+        this.status = status;
+    }
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 }
